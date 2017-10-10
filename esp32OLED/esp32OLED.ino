42df34431a7f3e3e73d6a1402eea1780032b1884
piramid <pornchai.tom@gmail.com>
2017-10-10 21:43:33 +0700

    Update esp32OLED.ino

--- esp32OLED/esp32OLED.ino
+++ esp32OLED/esp32OLED.ino
@@ -1,22 +1,4 @@
-/*
-  06/01/2016
-  Author: Makerbro
-  Platforms: ESP8266
-  Language: C++
-  File: HelloOLED.ino
-  ------------------------------------------------------------------------
-  Description: 
-  Demo for OLED display showcasing writing text to the screen.
-  ------------------------------------------------------------------------
-  Please consider buying products from ACROBOTIC to help fund future
-  Open-Source projects like this! We'll always put our best effort in every
-  project, and release all our design files and code for you to use. 
-  https://acrobotic.com/
-  ------------------------------------------------------------------------
-  License:
-  Released under the MIT license. Please check LICENSE.txt for more
-  information.  All text above must be included in any redistribution. 
-*/
+/*  Power by Mr. Pornchai Thong-in */
 #include <Wire.h>
 #include <ACROBOTIC_SSD1306.h>
 
